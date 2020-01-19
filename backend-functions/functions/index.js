f275205ac1cab07f3576ef68b3963af40648ffbd
Taiwo Ogunola <uniqueomokenny@gmail.com>
2020-01-19 01:08:14 +0100

    initial commit

---
+++ backend-functions/functions/index.js
@@ -0,0 +1,52 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send('Hello from Firebase!');
+});
+
+exports.getScreams = functions.https.onRequest((req, res) => {
+  admin
+    .firestore()
+    .collection('screams')
+    .get()
+    .then(data => {
+      let = screams = [];
+      data.forEach(doc => {
+        screams.push(doc.data());
+      });
+
+      return res.json(screams);
+    })
+    .catch(err => console.error(err));
+});
+
+exports.createScream = functions.https.onRequest((req, res) => {
+  if (req.method !== 'POST')
+    res.status(400).json({ error: 'method not allowed!' });
+
+  const { body, userHandle } = req.body;
+  const newScream = {
+    body,
+    userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  };
+
+  admin
+    .firestore()
+    .collection('screams')
+    .add(newScream)
+    .then(doc => {
+      console.log(doc);
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Something went wrong' });
+      console.error(err);
+    });
+});
