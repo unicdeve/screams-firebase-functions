24ff693667f6ec5673a0b3755564056b029df773
Taiwo Ogunola <uniqueomokenny@gmail.com>
2020-01-19 01:30:53 +0100

    added express to manage routes

---
+++ backend-functions/dbschema.js
@@ -0,0 +1,8 @@
+let db = {
+  screams: {
+    userHandle: 'user',
+    createdAt: 'ISOString Date',
+    likeCount: 5,
+    commentCount: 2
+  }
+};
--- backend-functions/functions/index.js
+++ backend-functions/functions/index.js
@@ -3,22 +3,25 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+const express = require('express');
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!');
-});
+const app = express();
 
-exports.getScreams = functions.https.onRequest((req, res) => {
+app.get('/screams', (req, res) => {
   admin
     .firestore()
     .collection('screams')
+    .orderBy('createdAt', 'desc')
     .get()
     .then(data => {
       let = screams = [];
       data.forEach(doc => {
-        screams.push(doc.data());
+        screams.push({
+          screamId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt
+        });
       });
 
       return res.json(screams);
@@ -26,15 +29,12 @@ exports.getScreams = functions.https.onRequest((req, res) => {
     .catch(err => console.error(err));
 });
 
-exports.createScream = functions.https.onRequest((req, res) => {
-  if (req.method !== 'POST')
-    res.status(400).json({ error: 'method not allowed!' });
-
+app.post('/scream', (req, res) => {
   const { body, userHandle } = req.body;
   const newScream = {
     body,
     userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    createdAt: new Date().toISOString()
   };
 
   admin
@@ -50,3 +50,5 @@ exports.createScream = functions.https.onRequest((req, res) => {
       console.error(err);
     });
 });
+
+exports.api = functions.https.onRequest(app);
