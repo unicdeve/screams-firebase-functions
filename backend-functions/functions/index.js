b84df534cbcc1d298cb1c9dff1e04efadfda1ee9
Taiwo Ogunola <uniqueomokenny@gmail.com>
2020-02-06 23:18:54 +0100

    firebase db triggers: notifications on like, comments

--- backend-functions/functions/index.js
+++ backend-functions/functions/index.js
@@ -1,8 +1,8 @@
 const functions = require('firebase-functions');
-
 const app = require('express')();
 
 const FBAuth = require('./util/fbAuth');
+const { db } = require('./util/admin');
 
 const {
   getAllScreams,
@@ -19,7 +19,9 @@ const {
   loginUser,
   uploadImage,
   addUserDetails,
-  getAuthenticatedUser
+  getAuthenticatedUser,
+  getUserDetails,
+  markNotificationsRead
 } = require('./handlers/users');
 
 // Screams route
@@ -37,5 +39,73 @@ app.post('/login', loginUser);
 app.post('/user/upload', FBAuth, uploadImage);
 app.post('/user', FBAuth, addUserDetails);
 app.get('/user', FBAuth, getAuthenticatedUser);
+app.get('/user/:handle', getUserDetails);
+app.post('/notifications', FBAuth, markNotificationsRead);
 
 exports.api = functions.https.onRequest(app);
+
+exports.createNotificationOnLike = functions.firestore
+  .document('likes/{id}')
+  .onCreate(snapshort => {
+    db.doc(`/screams/${snapshort.data().screamId}`)
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshort.id}`).set({
+            created: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshort.data().userHandle,
+            type: 'like',
+            read: false,
+            screamId: doc.id
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch(err => {
+        console.error(err);
+        return;
+      });
+  });
+
+exports.deleteNotificationOnUnlike = functions.firestore
+  .document('likes/{id}')
+  .onDelete(snapshort => {
+    db.doc(`/notifications/${snapshort.id}`)
+      .delete()
+      .then(() => {
+        return;
+      })
+      .catch(err => {
+        console.error(err);
+        return;
+      });
+  });
+
+exports.createNotificationOnComment = functions.firestore
+  .document('comments/{id}')
+  .onCreate(snapshort => {
+    db.doc(`/screams/${snapshort.data().screamId}`)
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshort.id}`).set({
+            created: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshort.data().userHandle,
+            type: 'comment',
+            read: false,
+            screamId: doc.id
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch(err => {
+        console.error(err);
+        return;
+      });
+  });
